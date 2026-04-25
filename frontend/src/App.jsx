2ee7233e092a6cd48ced364f93639174240bd82c
Avinash829 <avinashpappala@gmail.com>
2026-04-25 21:29:24 +0530

    cursor updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import Dashboard from "./pages/Dashboard";
-import CustomCursor from "./components/CustomCursor";
 
 function App() {
     return (
         <>
-            <CustomCursor />
             <Dashboard />
         </>
     );
