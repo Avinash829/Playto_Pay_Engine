c3af8e35f967b88e88c3bda413d468f990a38bb4
Avinash829 <avinashpappala@gmail.com>
2026-04-25 21:12:53 +0530

    remove redundant shell command from build script

--- backend/build.sh
+++ backend/build.sh
@@ -6,8 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Automatically seed data (safe to run multiple times)
+# Automatically seed data and schedule cron jobs
 python seed_data.py
-
-# Automatically schedule the background sweeper
-python manage.py shell -c "from payouts.models import Schedule; Schedule.objects.get_or_create(func='payouts.tasks.retry_stuck_payouts', defaults={'schedule_type': Schedule.MINUTES, 'minutes': 1, 'repeats': -1})"
